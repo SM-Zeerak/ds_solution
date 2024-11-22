cc73b3d193059614c2c2f055ec9c4344785b3e8c
SM-Zeerak <syedzeerak67@gmail.com>
2024-11-22 14:24:47 +0500

    Fix vendorRegister route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { createUser, getAllUsers } = require('../controllers/userController');
 const router = express.Router();
 
-router.post('/users', createUser); // Create a new user
+router.post('/vendorsRegister', createUser); // Create a new user
 router.get('/users', getAllUsers); // Get all users
 
 module.exports = router;
