3d4b73af2934800380f89fc3c6c0a78939689a18
SM-Zeerak <syedzeerak67@gmail.com>
2024-11-22 14:13:28 +0500

    Fix vendorRegister route

--- controllers/Vendor/auth/vendorController.js
+++ controllers/Vendor/auth/vendorController.js
@@ -2,14 +2,27 @@
 const vendor = require('../../../models/Vendor/Auth/vendorRegisterModel');
 
 // Create a new vendor
+// exports.createvendor = async (req, res) => {
+//   try {
+//     const { name, email, password , phone , address } = req.body;
+
+//     const vendor = new vendor({ name, email, password , phone , address });
+//     await vendor.save();
+//     res.status(201).json({ message: 'Vendor created successfully', vendor });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// };
+
 exports.createvendor = async (req, res) => {
   try {
     const { name, email, password , phone , address } = req.body;
 
-    const vendor = new vendor({ name, email, password , phone , address });
+    const vendor = new Vendor({ name, email, password , phone , address });
     await vendor.save();
     res.status(201).json({ message: 'Vendor created successfully', vendor });
   } catch (error) {
+    console.error(error);  // Log the full error
     res.status(500).json({ error: error.message });
   }
 };
